30ddca1320e383d9aaa768a8ecdac03c2ce20959
Debarnab Banerjee <banerjee.debarnab@gmail.com>
2018-04-20 20:10:33 +0200

    test commit

--- src/test/java/com/debarnab/cucumber/BDDTest.java
+++ src/test/java/com/debarnab/cucumber/BDDTest.java
@@ -1,7 +1,12 @@
 package com.debarnab.cucumber;
+
+import java.io.*;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -9,7 +14,17 @@ import cucumber.api.junit.Cucumber;
         , glue = {"com.debarnab.cucumber.testSteps"}
         , format = {"pretty", "html:target/site/cucumber-pretty", "json:target/cucumber.json"}
         , tags = {"~@Ignore"}
-        // , tags = {"@FilterOfSearchResults"}
+        ,plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}
 )
 public class BDDTest {
+    @AfterClass
+    public static void setup() {
+        Reporter.loadXMLConfig(System.getProperty("user.dir")+"//extent-config.xml");
+//        Reporter.setSystemInfo("user", System.getProperty("user.name"));
+//        Reporter.setSystemInfo("os", "Mac OSX");
+//        Reporter.setTestRunnerOutput("Sample test runner output message");
+    }
+
+
+
 }
